f914dceda650aae8562b1cf1dbdfe8e815534e63
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-19 13:50:38 +0700

    het video 1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import FadeView from './components/FadeView';
+
+export default class App extends Component {
+    render() {
+        return (
+            <View
+                style={{ 
+                    flex: 1, 
+                    justifyContent: 'center', 
+                    alignItems: 'center',
+                    backgroundColor: 'lightblue'
+                }}
+            >
+                <FadeView />
+            </View>
+        );
+    }
+}
